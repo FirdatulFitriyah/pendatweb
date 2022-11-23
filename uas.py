06adba077072f57c8e09aa4cc049372e9f12f908
FirdatulFitriyah <83995446+FirdatulFitriyah@users.noreply.github.com>
2022-11-23 19:14:45 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -21,7 +21,7 @@ from sklearn.utils.validation import joblib
 
 
 
-st.title("PENAMBANGAN DATA")
+st.title("DATA MINING")
 st.write("By: Firdatul Fitriyah - 200411100020")
 st.write("Grade: Penambangan Data C")
 upload_data, preporcessing, modeling, implementation = st.tabs(["Upload Data", "Prepocessing", "Modeling", "Implementation"])
