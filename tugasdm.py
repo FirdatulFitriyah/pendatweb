ad1cf6254b6c3b0718222e23a852ae70a0cde11f
FirdatulFitriyah <83995446+FirdatulFitriyah@users.noreply.github.com>
2022-11-24 07:11:09 +0700

    Update tugasdm.py

--- tugasdm.py
+++ tugasdm.py
@@ -24,7 +24,7 @@ from sklearn.utils.validation import joblib
 st.title("PENAMBANGAN DATA")
 st.write("By: Firdatul Fitriyah - 200411100020")
 st.write("Grade: Penambangan Data C")
-upload_data, preporcessing, modeling, implementation = st.tabs(["Upload Data", "Prepocessing", "Modeling", "Implementation"])
+upload_data, preporcessing, modeling,evaluation, implementation = st.tabs(["Upload Data", "Prepocessing", "Modeling", "evaluation","Implementation"])
 
 
 with upload_data:
