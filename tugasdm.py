e157eeaeceed87dd013abeacf5341d14d9470378
FirdatulFitriyah <83995446+FirdatulFitriyah@users.noreply.github.com>
2022-11-27 10:03:13 +0700

    Update tugasdm.py

--- tugasdm.py
+++ tugasdm.py
@@ -64,11 +64,11 @@ with preporcessing:
     st.write("""# Preprocessing""")
     df[["ID", "N_Days", "Status", "Drug", "Age", "Sex", "Ascites", "Hepatomegaly", "Spiders","Edema","Bilirubin","Cholesterol","Albumin","Copper","Alk_Phos","SGOT","Tryglicerides","Platelets","Prothrombin","Stage"]].agg(['min','max'])
 
-    df.sl.value_counts()
+    df.Stage.value_counts()
     # df = df.drop(columns=["date"])
 
-    X = df.drop(columns="sl")
-    y = df.sl
+    X = df.drop(columns="Stage")
+    y = df.Stage
     "### Membuang fitur yang tidak diperlukan"
     df
 
@@ -81,7 +81,7 @@ with preporcessing:
 
     le.inverse_transform(y)
 
-    labels = pd.get_dummies(df.sl).columns.values.tolist()
+    labels = pd.get_dummies(df.Stage).columns.values.tolist()
 
     "### Label"
     labels
@@ -96,7 +96,7 @@ with preporcessing:
 
     le.inverse_transform(y)
 
-    labels = pd.get_dummies(df.sl).columns.values.tolist()
+    labels = pd.get_dummies(df.Stage).columns.values.tolist()
     
     "### Label"
     labels
