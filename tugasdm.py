e14ddabc31d7e0e0f017b4d217341e5a0899e3ca
FirdatulFitriyah <83995446+FirdatulFitriyah@users.noreply.github.com>
2022-11-27 09:40:41 +0700

    Update tugasdm.py

--- tugasdm.py
+++ tugasdm.py
@@ -62,7 +62,7 @@ with upload_data:
 
 with preporcessing:
     st.write("""# Preprocessing""")
-    df[["ID", "N_Days", "Status", "Drug", "Age", "Sex", "Ascites", "Hepatomegaly", "Spiders","Edema","Bilirubin","Cholesterol","Albumin","Copper","Alk_Phos","SGOT","Tryglicerides","Platelets","Prothrombin"]].agg(['min','max'])
+    df[["N_Days", "Status", "Drug", "Age", "Sex", "Ascites", "Hepatomegaly", "Spiders","Edema","Bilirubin","Cholesterol","Albumin","Copper","Alk_Phos","SGOT","Tryglicerides","Platelets","Prothrombin","Stage"]].agg(['min','max'])
 
     df.Stage.value_counts()
     # df = df.drop(columns=["date"])
